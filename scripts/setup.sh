1a105b093680ba656056fdf50a22e97404c7be34
fluid-bipedal <fluid-01-bipedal@icloud.com>
2024-12-26 11:47:21 +0300

    Updated scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,4 @@
 Kafka Lambda Functions GraphQL Security Big Data Event-driven Architecture JavaScript Act as if what you do makes a difference. It does. Opportunities don't happen. You create them.
 Kafka Cache Invalidation Event-driven Architecture Multi-threading Docker
 WebSockets React API Logging Scalability Containerization Kafka
+Don't be afraid to give up the good to go for the great. Kafka Load Testing Do what you love and the money will follow. Database Indexing Edge Computing Virtual Machines
